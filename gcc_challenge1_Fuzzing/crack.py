14dadc321ca25532aa4ccd2ba9e65a73dd08cd5f
yasm1 <mmttt202@gmail.com>
2019-11-27 17:53:50 +0900

    Run angr in STRICT_PAGE_MODE to detect SEGV
    - But forvige SEGVs in libc functions
    - Still have bugs when forgiving libc functions
    - Using `forgive_symbol_segv` for some functions will make the script unfinishable

--- gcc_challenge1_Fuzzing/crack.py
+++ gcc_challenge1_Fuzzing/crack.py
@@ -4,19 +4,46 @@ import json
 
 from pathlib import Path
 
-def should_stop(proj, state, stop_by):
-    bb = proj.factory.block(state.solver.eval(state.regs.rip))
-    return bb.addr <= stop_by < bb.addr + bb.size
 
+def forgive_symbol_segv(proj, symbol):
+    sim_proc = None
+    if proj.is_symbol_hooked(symbol):
+        sim_proc = proj.hooked_by(proj.loader.find_symbol(symbol).rebased_addr)
 
-def run_stopping_by(proj, state, stop_by):
-    simgr = proj.factory.simgr(state)
+    class ForgiveSegv(angr.SimProcedure):
+        IS_FUNCTION = True
+        local_vars = ('sim_proc_class', 'symbol_name')
 
-    while simgr.active:
-        simgr = simgr.step()
-        simgr.move(from_stash='active', to_stash='stops', \
-                filter_func=lambda s: should_stop(proj, s, stop_by))
-    return simgr.stops
+        sim_proc_class = sim_proc.__class__
+        symbol_name = symbol
+
+        def run(self):
+            args = []
+            for i in range(7):
+                try:
+                    args.append(self.arg(i))
+                except angr.SimProcedureArgumentError:
+                    break
+
+            self.project.unhook_symbol(self.symbol_name)
+            # switch on STRICT_PAGE_ACCESS incase it is already off.
+            # remove will cause error on removing unexisting switch.
+            # self.state.options.update({angr.options.STRICT_PAGE_ACCESS})
+            self.state.options.remove(angr.options.STRICT_PAGE_ACCESS)
+
+            if self.sim_proc_class is None:
+                self.call(self.state.solver.eval(self.state.regs.rip),
+                          args, continue_at='enable_strict')
+            else:
+                ret = self.inline_call(self.sim_proc_class, *args).ret_expr
+                self.state.options.update({angr.options.STRICT_PAGE_ACCESS})
+                return ret
+
+        def enable_strict(self):
+            self.project.hook_symbol(self.symbol_name, ForgiveSegv())
+            self.state.options.update({angr.options.STRICT_PAGE_ACCESS})
+
+    proj.hook_symbol(symbol, ForgiveSegv())
 
 
 FILE_NAME = '/tmp/input.bin'
@@ -25,26 +52,33 @@ def crack(binpath):
     proj = angr.Project(binpath)
 
     argv = [binpath, FILE_NAME]
-    state = proj.factory.full_init_state(args=argv)
+    state = proj.factory.full_init_state(args=argv,
+                                         add_options={angr.options.STRICT_PAGE_ACCESS})
 
     # TODO: Maybe I shouldn't spcify concrete size.
     # Should size also be an data to solve dynamically?
     content = claripy.BVS('input', 200 * 8)
     input_file = angr.SimFile(FILE_NAME, content=content)
-    # input_file = angr.SimFile(FILE_NAME, size=200)
     state.fs.insert(FILE_NAME, input_file)
+
+    # forgive segmentation faults in libc functions
+    forgive_symbol_segv(proj, 'fopen')
+    forgive_symbol_segv(proj, 'fprintf')
+    forgive_symbol_segv(proj, 'malloc')
+    forgive_symbol_segv(proj, 'fclose')
+    # forgive_symbol_segv(proj, 'free') # will not finish
+    # forgive_symbol_segv(proj, 'fseek') # will not finish
 
     simgr = proj.factory.simgr(state)
     # run to call to function crash
-    stops = run_stopping_by(proj, state, BASE_ADDR+0xa4a)
     simgr.run()
 
-    if (len(stops) > 0):
+    if (len(simgr.errored) > 0):
         return (c \
-                for s in stops \
-                for c in s.solver.eval_upto(content, 1, cast_to=bytes))
+                for e in simgr.errored \
+                for c in e.state.solver.eval_upto(content, 1, cast_to=bytes))
     else:
-        raise Exception("Not found")
+        raise Exception("No crash detected")
 
 
 if __name__ == '__main__':
@@ -53,4 +87,3 @@ if __name__ == '__main__':
     for i, ci in enumerate(crash_inputs):
         with (p / '{}.bin'.format(i)).open('wb') as f:
             f.write(ci)
-    # print(json.dumps(list(crash_inputs), indent=4))
