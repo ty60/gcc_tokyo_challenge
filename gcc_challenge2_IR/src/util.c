91efb717189d639586c4f897c47f2ea7c015801c
yasm1 <mmttt202@gmail.com>
2019-12-01 16:22:24 +0900

    Write program to hexdump the latest backup of container superblock

---
+++ gcc_challenge2_IR/src/util.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void hexdump(const void* data, size_t size) {
+	char ascii[17];
+	size_t i, j;
+	ascii[16] = '\0';
+	for (i = 0; i < size; ++i) {
+		printf("%02X ", ((unsigned char*)data)[i]);
+		if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
+			ascii[i % 16] = ((unsigned char*)data)[i];
+		} else {
+			ascii[i % 16] = '.';
+		}
+		if ((i+1) % 8 == 0 || i+1 == size) {
+			printf(" ");
+			if ((i+1) % 16 == 0) {
+				printf("|  %s \n", ascii);
+			} else if (i+1 == size) {
+				ascii[(i+1) % 16] = '\0';
+				if ((i+1) % 16 <= 8) {
+					printf(" ");
+				}
+				for (j = (i+1) % 16; j < 16; ++j) {
+					printf("   ");
+				}
+				printf("|  %s \n", ascii);
+			}
+		}
+	}
+}
+
+
+void *xmalloc(size_t size)
+{
+    void *p = malloc(size);
+    if (!p) {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    }
+    return p;
+}
+
+
+void xfree(void *p)
+{
+    free(p);
+}
---
+++ gcc_challenge2_IR/src/util.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+void hexdump(const void* data, size_t size);
+void *xmalloc(size_t size);
+void xfree(void *p);
